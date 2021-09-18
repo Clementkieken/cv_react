3da4da25a8913c42296cb9bd8b6e388c433d8550
Clement Kieken <ckieken@protonmail.com>
2021-09-18 19:17:30 +0200

    formation et skills done mais pb flex

--- src/components/Formation/index.js
+++ src/components/Formation/index.js
@@ -2,41 +2,42 @@ import './styles.scss';
 import PropTypes from 'prop-types';
 
 const Formation = ({ refFormation }) => (
-  <div className="formations__main" ref={refFormation}>
-    <h1>Formations</h1>
+  <div className="formations" ref={refFormation}>
+    <h1 className="formations__title">Formations</h1>
+    <div className="formations__all">
+      <div className="formations__details">
+        <h2 className="formations__details__h2">Développeur Web Full Stack JavaScript React</h2>
+        <h3 className="formations__details__h3">O'Clock - Mai 2021 à octobre 2021</h3>
+        <div className="formations__details_paragraphe">
+          <p className="formations__details__p"> Pendant 4 mois nous avons appris les bases du code avec comme langage principal JavaScript que ce soit en front (JS Vanilla) ou en back (NodeJS mais aussi la BDD avec PostgreSQL). Chaque jour nous avons appris de nouvelles notions et le soir nous avions des exercices concernant à la fois les notions du jour et également une notion à découvrir par nous même afin de nous familiariser avec la recherche sur la documentation.</p>
+          <p className="formations__details__p">Puis pendant un mois nous nous sommes spécialisés en spé React où nous avons vu également Redux.</p>
+          <p className="formations__details__p">Enfin, lors du dernier mois, nous nous sommes séparés en plusieurs groupe de 4 à 5 étudiant où nous avons créé un site web responsive mobile. Notre projet, "Evadia", permet de condenser de multiples API sur des communes afin d'avoir toutes les informations nécessaires quand vous déménagez ou partez en week-end dans un lieu. Voir les détails dans l'onglet Projets.</p>
+        </div>
+      </div>
 
-    <div className="formations__details">
-      <h2>Développeur Web Full Stack JavaScript React</h2>
-      <h3>O'Clock - Mai 2021 à octobre 2021</h3>
-      <ul>
-        <li>React</li>
-        <li>JavaScript</li>
-        <li>NodeJS</li>
-        <li>MySQL</li>
-        <li>VueJS</li>
-        <li>Express</li>
-        <li>Sequelize</li>
-        <li>HTML/CSS</li>
-      </ul>
+      <div className="formations__details__transition" />
+
+      <div className="formations__details">
+        <h2 className="formations__details__h2">Chef Monteur de niveau 3</h2>
+        <h3 className="formations__details__h3">Insitut de l'Image et du Son (3iS) - 2010 à 2013</h3>
+        <ul>
+          <li className="formations__details__li">Montage</li>
+          <li className="formations__details__li">Prise et traitement Son</li>
+          <li className="formations__details__li">Post-production</li>
+          <li className="formations__details__li">Image (lumière, caméra)</li>
+        </ul>
+      </div>
+
+      <div className="formations__details__transition" />
+
+      <div className="formations__details">
+        <h2 className="formations__details__h2">Bacalauréat</h2>
+        <h3 className="formations__details__h3">EABJM - 2006 à 2009</h3>
+        <ul>
+          <li className="formations__details__li">BAC L option philosophie / anglais.</li>
+        </ul>
+      </div>
     </div>
-
-    <div className="formations__details">
-      <h2>Chef Monteur de niveau 3</h2>
-      <h3>Insitut de l'Iamge et du Son (3iS) - 2010 à 2013</h3>
-      <ul>
-        <li>Montage</li>
-        <li>Prise et traitement Son</li>
-        <li>Post-production</li>
-        <li>Image (lumière, caméra)</li>
-      </ul>
-    </div>
-
-    <div className="formations__details">
-      <h2>Bacalauréat</h2>
-      <h3>EABJM - 2006 à 2009</h3>
-      <p>BAC L option philosophie / anglais.</p>
-    </div>
-
   </div>
 );
 
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,52 +1,109 @@
 import './styles.scss';
 import PropTypes from 'prop-types';
 
+// Import logo
+
+import reactlogo from '../../../public/img/react.ico';
+import htmllogo from '../../../public/img/html.ico';
+import csslogo from '../../../public/img/css.ico';
+import jslogo from '../../../public/img/js.ico';
+import reduxlogo from '../../../public/img/redux.ico';
+import nodelogo from '../../../public/img/nodejs.ico';
+import postlogo from '../../../public/img/postgresql.ico';
+import ejslogo from '../../../public/img/ejs.ico';
+import expresslogo from '../../../public/img/express.png';
+import gitlogo from '../../../public/img/github.ico';
+
+import aelogo from '../../../public/img/ae.ico';
+import prlogo from '../../../public/img/pr.ico';
+import pslogo from '../../../public/img/ps.ico';
+import fcplogo from '../../../public/img/fcp.ico';
+import melogo from '../../../public/img/me.ico';
+import avidlogo from '../../../public/img/avid.ico';
+import shutterlogo from '../../../public/img/shutter.ico';
+
+import englogo from '../../../public/img/engflag.ico';
+import frlogo from '../../../public/img/frflag.ico';
+
+import wordlogo from '../../../public/img/word.ico';
+import excellogo from '../../../public/img/excel.ico';
+import sflogo from '../../../public/img/salesforce.ico';
+import gdoclogo from '../../../public/img/gdoc.ico';
+
 const Skills = ({ refSkills }) => (
-  <div className="skills__main" ref={refSkills}>
-    <h1>Compétences</h1>
+  <div className="skills" ref={refSkills}>
+    <h1 className="skills__title">Compétences</h1>
 
-    <div className="skills__detail">
-      <h2>Développeur</h2>
-      <ul>
-        <li>React</li>
-        <li>JavaScript</li>
-        <li>NodeJS</li>
-        <li>MySQL</li>
-        <li>VueJS</li>
-        <li>Express</li>
-        <li>Sequelize</li>
-        <li>HTML/CSS</li>
-      </ul>
+    <div className="skills__all">
+
+      <div className="skills__detail">
+        <h2 className="skills__detail__h2">Développeur</h2>
+        <div className="skills__details__logo_display">
+          <ul className="skills__details__ul">
+            <li className="skills__details__li_logo"><img className="formation__details__dev_logo" src={htmllogo} alt="html5 logo" /></li>
+            <li className="skills__details__li_logo"><img className="formation__details__dev_logo" src={csslogo} alt="css3 logo" /></li>
+            <li className="skills__details__li_logo"><img className="formation__details__dev_logo" src={reactlogo} alt="react logo" /></li>
+            <li className="skills__details__li_logo"><img className="formation__details__dev_logo" src={reduxlogo} alt="redux logo" /></li>
+            <li className="skills__details__li_logo"><img className="formation__details__dev_logo" src={jslogo} alt="javascript logo" /></li>
+            <li className="skills__details__li_logo"><img className="formation__details__dev_logo" src={nodelogo} alt="nodejs logo" /></li>
+            <li className="skills__details__li_logo"><img className="formation__details__dev_logo" src={postlogo} alt="postgres logo" /></li>
+            <li className="skills__details__li_logo"><img className="formation__details__dev_logo" src={ejslogo} alt="ejs logo" /></li>
+            <li className="skills__details__li_logo"><img className="formation__details__dev_logo" src={expresslogo} alt="express logo" /></li>
+            <li className="skills__details__li_logo"><img className="formation__details__dev_logo" src={gitlogo} alt="github logo" /></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="skills__detail__transition" />
+
+      <div className="skills__detail">
+        <h2 className="skills__detail__h2">Montage audiovisuel</h2>
+        <ul className="skills__detail_ul">
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={prlogo} alt="Adobe Premiere Pro logo" /></li>
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={pslogo} alt="Adobe PhotoShop logo" /></li>
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={aelogo} alt="After Effect logo" /></li>
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={avidlogo} alt="Avid logo" /></li>
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={fcplogo} alt="FCP 7 logo" /></li>
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={melogo} alt="Media Encoder logo" /></li>
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={shutterlogo} alt="Shutter Encoder logo" /></li>
+        </ul>"
+      </div>
+
+      <div className="skills__detail__transition" />
+
+      <div className="skills__detail">
+        <h2 className="skills__detail__h2">Bureautique</h2>
+        <ul className="skills__detail__ul">
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={wordlogo} alt="Microsoft Word logo" /></li>
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={excellogo} alt="Microsoft Excel logo" /></li>
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={gdoclogo} alt="Google doc sheet logo" />/</li>
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={sflogo} alt="SalesForce logo" /></li>
+        </ul>
+      </div>
+
+      <div className="skills__detail__transition" />
+
+      <div className="skills__detail">
+        <h2 className="skills__detail__h2">Langue</h2>
+        <ul className="skills__detail__ul">
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={englogo} alt="English flag logo" /></li>
+          <li className="skills__detail__li"><img className="formation__details__dev_logo" src={frlogo} alt="French flag logo" /></li>
+        </ul>
+      </div>
+
+      <div className="skills__detail__transition" />
+
+      <div className="skills__detail">
+        <h2 className="skills__detail__h2">Divers</h2>
+        <ul className="skills__detail__ul">
+          <li className="skills__detail__li">Auteur du mémoire "La Place du Monteur dans l'eSport" en 2013.</li>
+          <li className="skills__detail__li">Management de guilde sur World of Warcraft depuis 14 ans (entre 15 et 30 joueurs).</li>
+          <li className="skills__detail__li">Bénévole sur tout contenu vidéo (clips, émissions,enseignement...)./</li>
+          <li className="skills__detail__li">Backpack trip.</li>
+
+        </ul>
+      </div>
     </div>
-
-    <div className="skills__detail">
-      <h2>Montage audiovisuel</h2>
-      <ul>
-        <li>Première Pro</li>
-        <li>Photoshop</li>
-        <li>After Effect</li>
-        <li>Avid</li>
-        <li>FCP 7</li>
-        <li>Media Encoder</li>
-        <li>Shutter Encoder</li>
-      </ul>
-    </div>
-
-    <div className="skills__detail">
-      <h2>Langue</h2>
-      <ul>
-        <li>Anglais</li>
-      </ul>
-    </div>
-
-    <div className="skills__detail">
-      <h2>Divers</h2>
-      <ul>
-        <li>Auteur du mémoire "La Place du Monteur dans l'eSport" en 2013.</li>
-        <li>Chef de guilde sur World of Warcraft depuis 10 ans.</li>
-      </ul>
-    </div>
-
   </div>
 );
 
