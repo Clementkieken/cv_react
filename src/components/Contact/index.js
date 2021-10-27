310e4933409bcdcb5268d77063a306d81a4d138c
Clément Kieken <ckieken@protonmail.com>
2021-10-28 01:39:46 +0200

    bug flexbox contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -14,20 +14,20 @@ const Contact = ({ refContact }) => (
     <div className="contact__details">
 
       <div className="contact__details__display">
-        <a href="mailto:ckieken@protonmail.com" target="_blank" rel="noreferrer">
+        <a className="contact__details__a" href="mailto:ckieken@protonmail.com" target="_blank" rel="noreferrer">
           <img className="contact__details__email" src={email} alt="logo email" />
         </a>
         {/* <p className="contact__details__p">ckieken@protonmail.com</p> */}
       </div>
 
       <div className="contact__details__display">
-        <a href="https://www.linkedin.com/in/clement-kieken-1504b168/" target="_blank" rel="noreferrer">
+        <a className="contact__details__a" href="https://www.linkedin.com/in/clement-kieken-1504b168/" target="_blank" rel="noreferrer">
           <img className="contact__details__linkedin" src={linkedin} alt="logo linkedin" />
         </a>
       </div>
 
       <div className="contact__details__display">
-        <a href="https://github.com/Clementkieken" target="_blank" rel="noreferrer">
+        <a className="contact__details__a" href="https://github.com/Clementkieken" target="_blank" rel="noreferrer">
           <img className="contact__details__github" src={github} alt="logo github" />
         </a>
       </div>
