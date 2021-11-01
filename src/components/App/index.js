1d45e67e6f555f30265badee7c51f3146b1e7d12
Clément Kieken <ckieken@protonmail.com>
2021-11-01 15:46:27 +0100

    nav change color on scroll and click

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
+/* eslint-disable max-len */
 // == Import
-import { useRef } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 // import { Route, Switch } from 'react-router-dom';
 import './styles.css';
@@ -23,6 +24,58 @@ const App = () => {
   const refProjets = useRef();
   const refContact = useRef();
 
+  const navBanner = useRef();
+
+  const [navColor, setNavColor] = useState('bckgrnd__white');
+
+  const handleScroll = () => {
+    // savoir où l'écran (window) est positionné en height par rapport à tout le document
+    //* par ex si le document fait 10000px de height, je veux savoir où je suis en train de regarder
+    // je compare cette valeur avec la position dans le document de chq section avec les ref
+    //* si je suis au 4800px, je vérifie si 4800px est compris entre le positionnement du début de la section ou sa fin
+    // je modif la couleur en fonction de ce que je veux (là il y a plusieurs façon de faire)
+    // console.log(e);
+    // console.log('app : ', refApp.current.getBoundingClientRect())
+    // console.log('formation : ', refFormation.current.getBoundingClientRect())
+
+    const { top: refAboutTop, height: refAboutHeight } = refAbout.current.getBoundingClientRect();
+    const { top: refExpeTop, height: refExpeHeight } = refExperiences.current.getBoundingClientRect();
+    const { top: refFormTop, height: refFormHeight } = refFormation.current.getBoundingClientRect();
+    const { top: refSkillsTop, height: refSkillsHeight } = refSkills.current.getBoundingClientRect();
+    const { top: refProjetsTop, height: refProjetsHeight } = refProjets.current.getBoundingClientRect();
+    const { top: refContactTop, height: refContactHeight } = refContact.current.getBoundingClientRect();
+
+    const { height: navHeight } = navBanner.current.getBoundingClientRect();
+
+    if ((refExpeTop - navHeight) <= 0 && (refExpeTop - navHeight) > (0 - refExpeHeight)) {
+      setNavColor('bckgrnd__black');
+    }
+
+    if ((refSkillsTop - navHeight) <= 0 && (refSkillsTop - navHeight) > (0 - refSkillsHeight)) {
+      setNavColor('bckgrnd__black');
+    }
+
+    if ((refContactTop - navHeight) <= 0 && (refContactTop - navHeight) > (0 - refContactHeight)) {
+      setNavColor('bckgrnd__black');
+    }
+
+    if ((refAboutTop - navHeight) <= 0 && (refAboutTop - navHeight) > (0 - refAboutHeight)) {
+      setNavColor('bckgrnd__white');
+    }
+
+    if ((refFormTop - navHeight) <= 0 && (refFormTop - navHeight) > (0 - refFormHeight)) {
+      setNavColor('bckgrnd__white');
+    }
+
+    if ((refProjetsTop - navHeight) <= 0 && (refProjetsTop - navHeight) > (0 - refProjetsHeight)) {
+      setNavColor('bckgrnd__white');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => handleScroll());
+  }, []);
+
   return (
     <div className="app">
 
@@ -33,6 +86,8 @@ const App = () => {
         refSkills={refSkills}
         refProjets={refProjets}
         refContact={refContact}
+        navColor={navColor}
+        navBanner={navBanner}
       />
       <About refAbout={refAbout} />
       <Experiences refExperiences={refExperiences} />
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,106 +1,105 @@
+/* eslint-disable no-nested-ternary */
 // import { NavLink } from 'react-router-dom';
 // import About from '../About';
 import './styles.scss';
-import { useRef } from 'react';
 import PropTypes from 'prop-types';
+// import { useInView } from 'react-intersection-observer';
 
 const Navbar = ({
-  refAbout, refExperiences, refFormation, refSkills, refProjets, refContact,
-}) => {
-  const navBanner = useRef();
-  return (
-    <nav className="menu" ref={navBanner}>
-      <ul className="menu__items">
+  refAbout, refExperiences, refFormation, refSkills, refProjets, refContact, navColor, navBanner,
+}) => (
+  <nav
+    className={`menu ${navColor}`}
+    ref={navBanner}
+  >
+    <ul className="menu__items">
 
-        <div
-          className="menu__item_one"
-          onClick={() => {
-            const top = refAbout.current.offsetTop;
-            window.scrollTo({ top: (top - navBanner.current.getBoundingClientRect().height), behavior: 'smooth' });
-          }}
+      <div
+        className="menu__item_one"
+        onClick={() => {
+          const top = refAbout.current.offsetTop;
+          window.scrollTo({ top: (top - navBanner.current.getBoundingClientRect().height), behavior: 'smooth' });
+        }}
+      >
+        <li
+          className="menu__li"
         >
-          <li
-            className="menu__li"
-          >
-            A propos
-          </li>
-        </div>
-
-        <div
-          className="menu__item_one"
-          onClick={() => {
-            const top = refExperiences.current.offsetTop;
-            window.scrollTo({ top: (top - navBanner.current.getBoundingClientRect().height), behavior: 'smooth' });
-          // Le -88 sert à décaler le component pour ne pas qu'il soit caché par la navBar
-          }}
+          A propos
+        </li>
+      </div>
+      <div
+        className="menu__item_one"
+        onClick={() => {
+          const top = refExperiences.current.offsetTop;
+          window.scrollTo({ top: (top - navBanner.current.getBoundingClientRect().height), behavior: 'smooth' });
+        }}
+      >
+        <li
+          className="menu__li"
         >
-          <li
-            className="menu__li"
-          >
-            Experiences
-          </li>
-        </div>
+          Experiences
+        </li>
+      </div>
 
-        <div
-          className="menu__item_one"
-          onClick={() => {
-            const top = refFormation.current.offsetTop;
-            window.scrollTo({ top: (top - navBanner.current.getBoundingClientRect().height), behavior: 'smooth' });
-          }}
+      <div
+        className="menu__item_one"
+        onClick={() => {
+          const top = refFormation.current.offsetTop;
+          window.scrollTo({ top: (top - navBanner.current.getBoundingClientRect().height), behavior: 'smooth' });
+        }}
+      >
+        <li
+          className="menu__li"
         >
-          <li
-            className="menu__li"
-          >
-            Formation
-          </li>
-        </div>
+          Formation
+        </li>
+      </div>
 
-        <div
-          className="menu__item_one"
-          onClick={() => {
-            const top = refSkills.current.offsetTop;
-            window.scrollTo({ top: (top - navBanner.current.getBoundingClientRect().height), behavior: 'smooth' });
-          }}
+      <div
+        className="menu__item_one"
+        onClick={() => {
+          const top = refSkills.current.offsetTop;
+          window.scrollTo({ top: (top - navBanner.current.getBoundingClientRect().height), behavior: 'smooth' });
+        }}
+      >
+        <li
+          className="menu__li"
         >
-          <li
-            className="menu__li"
-          >
-            Compétences
-          </li>
-        </div>
+          Compétences
+        </li>
+      </div>
 
-        <div
-          className="menu__item_one"
-          onClick={() => {
-            const top = refProjets.current.offsetTop;
-            window.scrollTo({ top: (top - navBanner.current.getBoundingClientRect().height), behavior: 'smooth' });
-          }}
+      <div
+        className="menu__item_one"
+        onClick={() => {
+          const top = refProjets.current.offsetTop;
+          window.scrollTo({ top: (top - navBanner.current.getBoundingClientRect().height), behavior: 'smooth' });
+        }}
+      >
+        <li
+          className="menu__li"
         >
-          <li
-            className="menu__li"
-          >
-            Projets
-          </li>
-        </div>
+          Projets
+        </li>
+      </div>
 
-        <div
-          className="menu__item_one"
-          onClick={() => {
-            const top = refContact.current.offsetTop;
-            window.scrollTo({ top: (top - navBanner.current.getBoundingClientRect().height), behavior: 'smooth' });
-          }}
+      <div
+        className="menu__item_one"
+        onClick={() => {
+          const top = refContact.current.offsetTop;
+          window.scrollTo({ top: (top - navBanner.current.getBoundingClientRect().height), behavior: 'smooth' });
+        }}
+      >
+        <li
+          className="menu__li"
         >
-          <li
-            className="menu__li"
-          >
-            Contact
-          </li>
-        </div>
+          Contact
+        </li>
+      </div>
 
-      </ul>
-    </nav>
-  );
-};
+    </ul>
+  </nav>
+);
 
 export default Navbar;
 
@@ -111,4 +110,6 @@ Navbar.propTypes = {
   refSkills: PropTypes.object.isRequired,
   refProjets: PropTypes.object.isRequired,
   refContact: PropTypes.object.isRequired,
+  navColor: PropTypes.string.isRequired,
+  navBanner: PropTypes.object.isRequired,
 };
